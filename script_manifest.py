ff2b87b694dab9bb87241ee4ce7ca1f39ce4e7d4
Kien Tuong Truong <kientuong@gmail.com>
2019-04-20 15:01:23 +0200

    Aggiornato lo script per il manifest: credenziali da file esterno e output meno verboso

--- script_manifest.py
+++ script_manifest.py
@@ -1,7 +1,13 @@
 import requests
 import json
 
+#SETTINGS
+
+verbosePrint = False
+outputFile = "manifest.json"
+
 #COSTANTI
+
 url = "https://api.github.com"
 urlMods = url + "/repos/spiritodellaforgia/forgiacraft2/contents/forgiacraft2/mods"
 urlBase = url + "/repos/spiritodellaforgia/forgiacraft2/contents"
@@ -9,13 +15,12 @@ urlConfig = url + "/repos/spiritodellaforgia/forgiacraft2/contents/forgiacraft2/
 urlAuth = url + "/repos/d/forgiacraft2"
 urlRate = url + "/rate_limit"
 
-user = "spiritodellaforgia@gmail.com"
-password = input("Inserisci password")
-
+user, password = [line.strip('\n') for line in open("password.txt")]
 
 def getNumberOfRequest():
     authResponse = requests.get(urlRate, auth=(user, password))
-    print(authResponse.content)
+    if verbosePrint:
+        print(authResponse.content)
     jsonResponse = authResponse.content.decode()
     j = json.loads(jsonResponse)
     return j['resources']['core']['limit']
@@ -31,19 +36,28 @@ def getTreeJson(homeUrl,pathsToVisit,outputJsonArray):
     skip = False
     for dirToVisit in pathsToVisit:
         skip = False
+        if dirToVisit == "forgiacraft2/mods":
+            print("Currently looking for mods")
+        if dirToVisit == "forgiacraft2/config":
+            print("Currently looking for configuration files")
+        if dirToVisit == "forgiacraft2/forge":
+            print("Currently looking for forge files")
         if dirToVisit == "forgiacraft2/mods/hats" or dirToVisit == "forgiacraft2/config/agricraft/json" or dirToVisit == "forgiacraft2/config/biomesoplenty/biomes":
-            print("--------------------------------------------")
-            print("SKIPPING PATH " + dirToVisit)
-            print("--------------------------------------------")
+            if verbosePrint:
+                print("--------------------------------------------")
+                print("SKIPPING PATH " + dirToVisit)
+                print("--------------------------------------------")
             continue
         jsonTreeResponse = requests.get(homeUrl + dirToVisit + '/',auth=(user, password))
         jsonTreeString  = jsonTreeResponse.content.decode()
         jsonTreeArray = json.loads(jsonTreeString)
-        print(jsonTreeArray)
+        if verbosePrint:
+            print(jsonTreeArray)
         for element in jsonTreeArray:
             if element['type'] == 'file':
                 elementStruct = {}
-                print("Found file: ",element['path'])
+                if verbosePrint:
+                    print("Found file: ",element['path'])
                 files = files + 1
                 elementStruct['path'] = element['path']
                 elementStruct['download_url'] = element['download_url']
@@ -53,22 +67,26 @@ def getTreeJson(homeUrl,pathsToVisit,outputJsonArray):
             else:
                 subFolders = subFolders + 1
                 newPaths = []
-                print("Found folder: ",element['path'])
+                if verbosePrint:
+                    print("Found folder: ",element['path'])
                 newPaths.append(element['path'])
                 getTreeJson(homeUrl,newPaths,outputJsonArray)
-    print("FOLDER ",dirToVisit," FINISHED")
-    print("NUMBER OF FILES FOUND:",files)
-    print("NUMBER OF FOLDERS FOUND:",subFolders)
-    print("--------------------------------------------")
+    if verbosePrint:
+        print("FOLDER ",dirToVisit," FINISHED")
+        print("NUMBER OF FILES FOUND:",files)
+        print("NUMBER OF FOLDERS FOUND:",subFolders)
+        print("--------------------------------------------")
     return json.dumps(outputJsonArray,indent=4)
 
 def getDirsHashesJson(homeUrl,dirsToSearch,outputJsonStruct):
     outputJsonStruct['hashes'] = []
     jsonString = requests.get(homeUrl, auth=(user,password)).content.decode()
-    print("--------------------------------------------")
+    if verbosePrint:
+        print("--------------------------------------------")
     jsonArray = json.loads(jsonString)
-    print("--------------------------------------------")
-    print(homeUrl)
+    if verbosePrint:
+        print("--------------------------------------------")
+        print(homeUrl)
     for dirName in dirsToSearch:
         for obj in jsonArray:
             if dirName == obj['name']:
@@ -85,11 +103,16 @@ dirs = ['mods','config','forge']
 dirs2 = ['/forgiacraft2']
 blacklist = ['forgiacraft2/config/agricraft/json','forgiacraft2/config/hats']
 getDirsHashesJson(urlBase + '/forgiacraft2/',dirs,ogg)
-print("JSON PARSER")
-print("NUMBER OF REQUEST REMAINING: ",getNumberOfRequest())
+if verbosePrint:
+    print("JSON PARSER")
+    print("NUMBER OF REQUEST REMAINING: ",getNumberOfRequest())
 ogg['files'] = []
 jsonOut = getTreeJson(urlBase,dirs2,ogg)
-print("SCAN COMPLETED\nHERE'S THE JSON:\n",jsonOut)
-f = open('out.json','w+')
+if verbosePrint:
+    print("SCAN COMPLETED\nHERE'S THE JSON:\n",jsonOut)
+else:
+    print("Scan completed: find the output in ", outputFile)
+    print("For a more detailed output, please change verbosePrint to True")
+f = open(outputFile,'w')
 f.write(jsonOut)
 f.close()
